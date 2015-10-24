b705d47e24bec03de5607aad7595f748e2ea1435
krishna kumar <krishnau2@gmail.com>
2015-10-24 23:55:29 +0530

    Setting up Reactjs with rails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Reactjs Integeration
+  config.react.variant = :production
+  config.react.addons = true
 end
