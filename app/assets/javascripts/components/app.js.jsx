f6565192ee85ae53673119aec5f2502f746734d1
krishna kumar <krishnau2@gmail.com>
2015-10-26 01:48:00 +0530

    - testing class name is replaced with real name - html table is generated by react and pre-fills with Board table data through ajax call in componentDidMount lifecycle method. - minimal css is added for UI

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -1,7 +1,48 @@
-var HelloMessage = React.createClass({
+var Bingo = React.createClass({
   render: function(){
     return(
-        <h3>Bingo!</h3>
-      )
+      <div className='board-container'>
+        <GameBoard />
+      </div>
+    );
   }
 });
+
+var GameBoard = React.createClass({
+  getInitialState: function(){    
+    return{
+      boardData: []
+    }
+  },
+  componentDidMount: function(){
+    $.ajax({
+      url: '/boards',
+      dataType: 'json',
+      success: function(data){
+        if (this.isMounted()) {
+          this.setState({boardData: data })
+        }
+      }.bind(this)
+    });
+  },
+  displayRow: function(row){
+    return(
+      <tr key={row.id}>
+        <td>{row.a}</td> 
+        <td>{row.b}</td> 
+        <td>{row.c}</td> 
+        <td>{row.d}</td> 
+        <td>{row.e}</td>
+      </tr>
+    );
+  },
+  render: function(){
+    return(
+      <table>
+        <tbody>
+          {this.state.boardData.map(this.displayRow)}
+        </tbody>
+      </table>
+    );
+  }
+})
